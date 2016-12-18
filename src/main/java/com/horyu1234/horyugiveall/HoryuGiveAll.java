d8e8780eeb4fba7d76e15c25bdd832c7c5a860a8
horyu1234 <horyu1234@naver.com>
2016-12-18 21:53:04 +0900

    Initial commit

---
+++ src/main/java/com/horyu1234/horyugiveall/HoryuGiveAll.java
@@ -0,0 +1,16 @@
+package com.horyu1234.horyugiveall;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by horyu on 2016-12-18.
+ */
+public class HoryuGiveAll extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
